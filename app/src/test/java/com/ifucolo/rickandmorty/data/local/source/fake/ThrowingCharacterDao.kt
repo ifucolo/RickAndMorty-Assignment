6bdba4b86fe416587a83b725c34b4e8ad5a68a7e
Iago Fucolo <iagofucolo@gmail.com>
2025-10-07 16:57:48 +0200

    test - ThrowingCharacterDao with real implementations

--- app/src/test/java/com/ifucolo/rickandmorty/data/local/source/fake/ThrowingCharacterDao.kt
+++ app/src/test/java/com/ifucolo/rickandmorty/data/local/source/fake/ThrowingCharacterDao.kt
@@ -7,6 +7,10 @@ class ThrowingCharacterDao : CharacterDao {
         override suspend fun getById(id: Int): CharacterEntity? {
             throw RuntimeException("boom-get")
         }
-        override suspend fun upsert(entity: CharacterEntity) = Unit
-        override suspend fun upsertAll(items: List<CharacterEntity>) = Unit
+        override suspend fun upsert(entity: CharacterEntity) {
+            throw RuntimeException("boom-upsert")
+        }
+        override suspend fun upsertAll(items: List<CharacterEntity>) {
+            throw RuntimeException("boom-upsertAll")
+        }
     }
